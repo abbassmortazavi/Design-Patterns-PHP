115a5c5e4455bb6d368261627c8b2cdf81409119
Abbass <abbassmortazavi@gmail.com>
2022-06-06 21:12:08 +0430

    push all files

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+//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
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createMutable(dirname(__DIR__));
+$dotenv->load();
+
+echo "hi";
+
+
+
+
